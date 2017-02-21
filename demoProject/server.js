8bc521b80af3547a35702182db85886436aaf8e0
akhileshwar.guli <akhileshwar.guli@gmail.com>
2017-02-20 18:58:03 -0600

    Added Comments

--- demoProject/server.js
+++ demoProject/server.js
@@ -2,27 +2,14 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 
-user = {
-  "user6" : {
-     "firstname" : "Sunny",
-     "lastname":"guntta",
-   "password" : "password1",
-   "paymentcard":{
-     "nameoncard": "Mahesh Guntta"
-     ,"card":"Amex"
-     ,"cardnumber":"37977553384896"
-     ,"expdate":"2112"
-     ,"cvv":"3413"
-   }
-  }
-}
-
 app.get('/index.html', function (req, res) {
    res.sendFile( __dirname + "/" + "index.html" );
 })
 
+// TODO: This function lists all the users
 app.get('/listUser', function(req, rres){});
 
+// This function returns User Information
 app.get('/getUser', function(req, res) {
 fs.readFile(__dirname+"/data/json/"+"users.json", 'utf8',
               function(err,data){
@@ -36,6 +23,7 @@ fs.readFile(__dirname+"/data/json/"+"users.json", 'utf8',
 
 });
 
+// This function Adds new users
 app.get('/addUser', function(req, res) {
   var data = readFile(__dirname+"/data/json/", "users.json");
   console.log("Reading the File ************************");
@@ -46,6 +34,7 @@ app.get('/addUser', function(req, res) {
   writeFile(__dirname+"/data/json/", "users.json", data);
 });
 
+// This function Add new Items to the specified cart
 app.get('/addItem', function(req,res){
   var cartID = "cart"+req.query.cartID;
   var itemID = "item"+req.query.upc;
@@ -71,6 +60,7 @@ app.get('/addItem', function(req,res){
   writeFile(__dirname+"/data/json/","carts.json", carts);
 });
 
+/* -------------------------- Utility function --------------------------*/
 var readFile = function(dir, fileName){
   var data = fs.readFileSync(dir+fileName);
   return data
@@ -85,6 +75,8 @@ var writeFile = function(dir, fileName, data){
               });
 };
 var sizeOf = function(jsonObj){ return Object.keys(jsonObj).length };
+
+/* -------------------------- Server Config --------------------------*/
 var server =app.listen(8081, function(){
   var host = server.address().address;
   var port = server.address().port;
